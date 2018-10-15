f1024d8a6652523db2ce3f905b660d078df75dc0
Parsa Moshiri <parsamoshiri5@gmail.com>
2018-10-15 14:21:46 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -5,4 +5,12 @@ class App < Sinatra::Base
   get '/name' do
     "My name is _"
   end
+  
+  get '/hometown' do
+    "My hometown is _"
+  end
+  
+  get '/favorite-song' do 
+    "My favorite song is _"
+  end
 end
